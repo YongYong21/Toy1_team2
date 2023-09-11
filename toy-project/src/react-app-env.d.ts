9b5ee927296ae2d16b8336ac493baa27a361f923
xxxjinn <tlsguswls319@naver.com>
2023-09-11 17:31:27 +0900

    feat: firebase 설정

--- toy-project/src/react-app-env.d.ts
+++ toy-project/src/react-app-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="react-scripts" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: "development" | "production";
+    REACT_APP_API_KEY: string;
+    REACT_APP_AUTH_DOMAIN: string;
+    REACT_APP_PROJECT_ID: string;
+    REACT_APP_STORAGE_BUCKET: string;
+    REACT_APP_MESSAGING_SENDER_ID: string;
+    REACT_APP_APP_ID: string;
+    REACT_APP_MEASUREMENT_ID: string;
+  }
+}
