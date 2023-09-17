6990f051fca5f53dd33576c2ed5982c90ff91fcf
YongYong21 <imkinggosu@gmail.com>
2023-09-18 05:30:43 +0900

    Feat: Update Firebase field
    필드 데이터값 수정 기능 추가

--- src/components/Contents.tsx
+++ src/components/Contents.tsx
@@ -29,11 +29,66 @@ const EditBtn = styled.button``;
 const ContentsDiv = styled.div`
   padding: 8px;
 `;
-
+// function 함수(): void {
+// 생성하는 함수
+// firestore.collection("sidebarMenu")
+//   .add({
+//     id: 0,
+//     title: "회사생활",
+//     items: [
+//       { text: "회사 내규", content: '회사 내규', timeStamp: '2023-09-08 15:00', url:'rule'},
+//       { text: "팀 소개", content: '팀 소개', timeStamp: '2023-09-08 15:00',url:'information'},
+//       { text: "조직도", content: '조직도', timeStamp: '2023-09-08 15:00',url:'team'}
+//     ]
+//   })
+//   .then((docRef) => {
+//     console.log("Document written with ID: ", docRef.id);
+//   })
+//   .catch((error) => {
+//     console.error("Error adding document: ", error);
+//   });
+// 가지고오기
+// var docRef = firestore.collection("sidebarMenu").doc("test");
+// docRef
+//   .get()
+//   .then((doc) => {
+//     if (doc.exists) {
+//       console.log("Document data:", doc.data());
+//     } else {
+//       // doc.data() will be undefined in this case
+//       console.log("No such document!");
+//     }
+//   })
+//   .catch((error) => {
+//     console.log("Error getting document:", error);
+//   });
+// }
+// 함수();
 function Contents(): JSX.Element {
   const { id } = useParams();
   const [data, setData] = useState<any | null>(null); // Use a more specific type if possible
-
+  const [idx, setIdx] = useState(0);
+  const [docName, setDocName] = useState('');
+  // let item = [
+  //   {
+  //     text: '회사 내규',
+  //     content: '회사 내규',
+  //     timeStamp: '2023-09-08 15:00',
+  //     url: 'rule',
+  //   },
+  //   {
+  //     text: '팀 소개',
+  //     content: '팀 소개',
+  //     timeStamp: '2023-09-08 15:00',
+  //     url: 'information',
+  //   },
+  //   {
+  //     text: '조직도',
+  //     content: '조직도',
+  //     timeStamp: '2023-09-08 15:00',
+  //     url: 'team',
+  //   },
+  // ];
   useEffect(() => {
     firestore
       .collection('sidebarMenu')
@@ -41,11 +96,12 @@ function Contents(): JSX.Element {
       .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
           const items = doc.data().items;
-          // let copy = [...db];
 
-          for (const key in items) {
-            if (items[key].url === id) {
-              setData(items[key]);
+          for (const [index, item] of items.entries()) {
+            if (item.url === id) {
+              setDocName(doc.id);
+              setIdx(index); // 해당 요소의 인덱스 출력
+              setData(item);
               return;
             }
           }
@@ -73,14 +129,41 @@ function Contents(): JSX.Element {
     const newText = e.target.value;
     setMarkdownText(newText);
   };
+
   const handleSaveClick = (): void => {
-    // console.log(firestore.collection("sidebarMenu").doc('Ry903jgEiuNn72Bz8NZ7'))
-    // firestore.collection("sidebarMenu").doc('Ry903jgEiuNn72Bz8NZ7').update({ [`items.0.content`]: MarkdownText})
-    // firestore.collection("sidebarMenu").doc('Oa1Mk5TW1Q4gYw2budOr').update({ [`items.0.content`]: "진행중인 프로젝트",
-    // [`items.0.text`]: "진행중인 프로젝트",
-    // [`items.0.timeStamp`]: "2023-09-08 15:00",
-    // [`items.0.url`]: "ongoing", })
-    // firestore.collection("sidebarMenu").doc(id).update({ content: markdownText })
+    // 업데이트할 문서의 참조 가져오기
+    const docRef = firestore.collection('sidebarMenu').doc(docName);
+    docRef
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          const data = doc.data();
+          const items = data?.items;
+
+          // content를 수정하려는 배열 요소의 인덱스 (예: 첫 번째 요소를 수정하려면 0)
+          const itemIndex = idx;
+
+          // 새로운 content 값으로 수정
+          items[itemIndex].content = markdownText;
+
+          // 수정된 items 배열을 다시 문서에 저장
+          docRef
+            .update({
+              items: items,
+            })
+            .then(() => {
+              console.log('content 수정 완료');
+            })
+            .catch((error) => {
+              console.error('content 수정 중 오류 발생: ', error);
+            });
+        } else {
+          console.log('문서가 존재하지 않습니다.');
+        }
+      })
+      .catch((error) => {
+        console.error('문서 가져오기 중 오류 발생: ', error);
+      });
   };
 
   return (
