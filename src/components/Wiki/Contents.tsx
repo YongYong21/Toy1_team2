67887639c60e897572c25d730524879aac4f09b9
YongYong21 <imkinggosu@gmail.com>
2023-09-21 19:23:47 +0900

    Fixed: 컴포넌트 분리 / 스크롤 버그 수정 / 404 페이지 수정

--- src/components/Wiki/Contents.tsx
+++ src/components/Wiki/Contents.tsx
@@ -1,65 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import { firestore } from '../../api/firebase';
+import React, { useEffect, useState, useRef } from 'react';
+
+import firebase, { firestore } from '../../api/firebase';
 import { useParams, useNavigate } from 'react-router-dom';
 import MarkdownPreview from '@uiw/react-markdown-preview';
-import { theme } from '../../styles/Theme';
 import { useAuthState } from '../../contexts/AuthContext';
+import { AiOutlineClose } from 'react-icons/ai';
 
-const ContentsContainer = styled.div`
-  min-width: 1080px;
-  width: 1280px;
-  padding: 20px 30px 0px;
-  margin-left: 256px;
-`;
-const TitleDiv = styled.div`
-  height: 52px;
-  font-size: 36px;
-  padding: 8px;
-  margin-bottom: 16px;
-`;
-const TimeStampWrap = styled.div`
-  display: flex;
-  margin-bottom: 16px;
-  justify-content: space-between;
-  align-items: center;
-`;
-const TimeStampDiv = styled.div`
-  width: 80%;
-  height: 30px;
-  font-size: 16px;
-  padding: 8px;
-`;
-const EditBtn = styled.button`
-  color: white;
-  min-width: 92px;
-  height: 48px;
-  font-size: ${theme.textStyles.button.fontSize};
-  line-height: ${theme.textStyles.button.lineHeight};
-  background-color: ${theme.blue700};
-  border-radius: 12px;
-  padding: 12px 32px;
-  transition: all 0.3s;
-  &:hover {
-    background-color: ${theme.blue800};
-    color: #fefefe;
-  }
-`;
-const ContentsDiv = styled.div`
-  padding: 8px;
-  ul {
-    list-style-type: disc;
-  }
-  ol {
-    list-style-type: decimal;
-  }
-`;
-
+import {
+  ContentsContainer,
+  TitleDiv,
+  TimeStampWrap,
+  TimeStampDiv,
+  EditBtn,
+  CancelBtn,
+  AddBtn,
+  ContentsDiv,
+  PostDiv,
+  PostTitleDiv,
+  DimDiv,
+  PostModal,
+  PostTitleContainer,
+  LabelDiv,
+  Label,
+  ButtonContainer,
+} from '../../styles/Wiki/ContentsSC';
 interface ItemType {
   text: string;
   content: string;
   timeStamp: string;
   url: string;
+}
+
+interface PostType {
+  title: string;
+  contents: string;
+  timeStamp: string;
+  author: string;
+  id: string;
 }
 
 function Contents(): JSX.Element {
@@ -69,45 +46,190 @@ function Contents(): JSX.Element {
   const [markdownText, setMarkdownText] = useState<string>('');
   const authState = useAuthState();
 
+  const [post, setPost] = useState<PostType[]>([]);
+  const [postState, setPostState] = useState(false);
+  const [postTitle, setPostTitle] = useState('');
+  const [postContents, setPostContents] = useState('');
+  const [username, setUsername] = useState<string>('사용자');
+  const [toggleAddBtn, setToggleAddBtn] = useState(false);
+
+  const outside = useRef(null);
   const navigate = useNavigate();
   // URL
-  const currentURL = window.location.href;
-  const newURL = currentURL
-    .replace('http://localhost:3000', '')
-    .concat('/edit');
-
   useEffect(() => {
-    firestore
-      .collection('sidebarMenu')
-      .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          const items = doc.data().items;
-
-          for (const item of items) {
-            if (item.url === id) {
-              setData(item);
-              setMarkdownText(item.content);
-              return;
-            }
-          }
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+    // 건의사항 페이지면
+    if (id === 'suggestions') {
+      const docRef = firestore.collection('post');
+      docRef
+        .get()
+        .then((querySnapshot) => {
+          const copy: PostType[] = [];
+          querySnapshot.forEach((doc) => {
+            copy.push(doc.data() as PostType);
+          });
+          setPost(copy);
+          setPostState(true);
+        })
+        .catch((error) => {
+          console.log('Error getting document:', error);
         });
-      })
-      .catch((error) => {
-        console.error('Error fetching menu data:', error);
+      // 유저 이름 가져오기
+      firebase.auth().onAuthStateChanged((user) => {
+        if (user !== null) {
+          const displayName = user.displayName;
+          setUsername(displayName ?? '사용자');
+        } else {
+          console.log('Signed Out'); // 로그인 안 됐을 때
+        }
       });
+    } else {
+      // 건의사항 페이지가 아니면 데이터 가져오기
+      firestore
+        .collection('sidebarMenu')
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            const items = doc.data().items;
+
+            for (const item of items) {
+              if (item.url === id) {
+                setData(item);
+                setMarkdownText(item.content);
+                setPostState(false);
+                return;
+              }
+            }
+          });
+        })
+        .catch((error) => {
+          console.error('Error fetching menu data:', error);
+        });
+    }
   }, [id]);
 
   // 글 수정 버튼 함수
   function handleClickEditBtn(): void {
     if (authState.state === 'loaded' && authState.isAuthentication) {
-      navigate(newURL);
+      navigate(`/wiki/${id}/edit`);
     } else if (authState.state === 'loaded' && !authState.isAuthentication) {
-      alert('글수정 기능은 로그인을 해야합니다.');
+      alert('해당 기능은 로그인을 해야합니다.');
     }
   }
+  // 글 작성 버튼 클릭
+  function handleClickAddBtn(): void {
+    // 로그인 확인
+    if (authState.state === 'loaded' && authState.isAuthentication) {
+      setToggleAddBtn(!toggleAddBtn);
+    } else if (authState.state === 'loaded' && !authState.isAuthentication) {
+      alert('해당 기능은 로그인을 해야합니다.');
+    }
+  }
+  // 모달 타이틀 입력
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setPostTitle(e.target.value);
+  };
+  // 모달 내용 입력
+  const handleContentsChange = (
+    e: React.ChangeEvent<HTMLTextAreaElement>,
+  ): void => {
+    setPostContents(e.target.value);
+  };
+  // 모달 글작성 클릭
+  const handlePostAddClick = (): void => {
+    // 로그인 했는지 확인
+    if (authState.state === 'loaded' && authState.isAuthentication) {
+      const now = new Date();
+      const year = now.getFullYear();
+      const month = String(now.getMonth() + 1).padStart(2, '0');
+      const day = String(now.getDate()).padStart(2, '0');
+      const hours = String(now.getHours()).padStart(2, '0');
+      const minutes = String(now.getMinutes()).padStart(2, '0');
+      const seconds = String(now.getSeconds()).padStart(2, '0');
+      const time = `${year}-${month}-${day} ${hours}:${minutes}`;
+      const timeId = `${year}${month}${day}${hours}${minutes}${seconds}`;
 
-  // 마크다운뷰어
+      const docRef = firestore.collection('post');
+      // 추가 함수
+      docRef
+        .add({
+          timeStamp: time,
+          author: username,
+          title: postTitle,
+          contents: postContents,
+          id: timeId,
+        })
+        .then(() => {
+          const newPost = {
+            timeStamp: time,
+            author: username,
+            title: postTitle,
+            contents: postContents,
+            id: timeId,
+          };
+          setPost([...post, newPost]);
+          setToggleAddBtn(false);
+          setPostTitle('');
+          setPostContents('');
+        })
+        .catch((error) => {
+          console.error('Error writing document: ', error);
+        });
+    } else if (authState.state === 'loaded' && !authState.isAuthentication) {
+      alert('해당 기능은 로그인을 해야합니다.');
+    }
+  };
+  // 모달 취소버튼 클릭
+  const handleCancelClick = (): void => {
+    setToggleAddBtn(false);
+    setPostTitle('');
+    setPostContents('');
+  };
+  // 모달 배경화면 클릭
+  const handleModalClose = (e: React.MouseEvent): void => {
+    if (toggleAddBtn && outside.current === e.target) {
+      setToggleAddBtn(false);
+    }
+  };
+  /**
+   * 삭제 버튼 클릭 핸들러
+   * @param {string} id - 삭제할 아이템의 ID
+   */
+  const handleDeleteClick = (id: string): void => {
+    firestore
+      .collection('post')
+      .where('id', '==', id)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          firestore
+            .collection('post')
+            .doc(doc.id)
+            .delete()
+            .then(() => {
+              const updatedPosts = post.filter(
+                (postItem) => postItem.id !== id,
+              );
+              setPost(updatedPosts);
+              console.log('Document successfully deleted!');
+            })
+            .catch((error) => {
+              console.error('Error removing document: ', error);
+            });
+        });
+      })
+      .catch((error) => {
+        console.log('Error getting documents: ', error);
+      });
+  };
+  /**
+   * @param {string} markdownText - 마크다운 텍스트
+   * @returns {JSX.Element} - 마크다운 뷰어 컴포넌트
+   */
   const MarkdownViewer = ({
     markdownText,
   }: {
@@ -119,18 +241,82 @@ function Contents(): JSX.Element {
       </div>
     );
   };
-
   return (
-    <ContentsContainer>
-      <TitleDiv>{data?.text}</TitleDiv>
-      <TimeStampWrap>
-        <TimeStampDiv>글작성 날짜: {data?.timeStamp}</TimeStampDiv>
-        <EditBtn onClick={handleClickEditBtn}>글 수정</EditBtn>
-      </TimeStampWrap>
-      <ContentsDiv>
-        <MarkdownViewer markdownText={markdownText} />
-      </ContentsDiv>
-    </ContentsContainer>
+    <>
+      {postState ? (
+        <>
+          {toggleAddBtn ? (
+            <DimDiv ref={outside} onClick={handleModalClose}>
+              <PostModal>
+                <PostTitleContainer>
+                  건의사항
+                  <AiOutlineClose onClick={handleCancelClick}></AiOutlineClose>
+                </PostTitleContainer>
+                <LabelDiv>
+                  <Label>
+                    <div>제목:</div>
+                    <input
+                      type="text"
+                      value={postTitle}
+                      onChange={handleTitleChange}
+                    />
+                  </Label>
+                  <br />
+                  <div>작성자: {username}</div>
+                  <Label>
+                    <div>내용:</div>
+                    <textarea
+                      value={postContents}
+                      onChange={handleContentsChange}
+                    />
+                  </Label>
+                </LabelDiv>
+
+                <ButtonContainer>
+                  <EditBtn onClick={handlePostAddClick}>글 작성</EditBtn>
+                  <CancelBtn onClick={handleCancelClick}>취소하기</CancelBtn>
+                </ButtonContainer>
+              </PostModal>
+            </DimDiv>
+          ) : (
+            ''
+          )}
+          <ContentsContainer>
+            {post.length === 0 ? <h1>건의사항 글이 없습니다.</h1> : ''}
+            <AddBtn onClick={handleClickAddBtn}>글 작성</AddBtn>
+            {post.map((v: any, i: number) => {
+              return (
+                <PostDiv key={i}>
+                  <PostTitleDiv>
+                    {v.title}
+                    <AiOutlineClose
+                      onClick={() => {
+                        handleDeleteClick(v.id);
+                      }}
+                    ></AiOutlineClose>
+                  </PostTitleDiv>
+                  <div>작성자: {v.author}</div>
+                  <div>작성일: {v.timeStamp}</div>
+                  <hr></hr>
+                  <div>{v.contents}</div>
+                </PostDiv>
+              );
+            })}
+          </ContentsContainer>
+        </>
+      ) : (
+        <ContentsContainer>
+          <TitleDiv>{data?.text}</TitleDiv>
+          <TimeStampWrap>
+            <TimeStampDiv>글작성 날짜: {data?.timeStamp}</TimeStampDiv>
+            <EditBtn onClick={handleClickEditBtn}>글 수정</EditBtn>
+          </TimeStampWrap>
+          <ContentsDiv>
+            <MarkdownViewer markdownText={markdownText} />
+          </ContentsDiv>
+        </ContentsContainer>
+      )}
+    </>
   );
 }
 
--- src/pages/Wiki/Wiki.tsx
+++ src/pages/Wiki/Wiki.tsx
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Sidebar from '../../components/Aside/Sidebar';
 import Content from '../../components/Wiki/Contents';
-
+import { Footer } from '../../components/Footer/Footer';
 import styled from 'styled-components';
+import { useParams, useNavigate } from 'react-router-dom';
 
 const FlexBox = styled.div`
+  min-height: 80vh;
   margin-top: 56px;
+  margin-bottom: 56px;
   display: flex;
 `;
 export default function Wiki(): JSX.Element {
   const url = 'wiki';
   const collectionName = 'sidebarMenu';
+
+  const page = [
+    'rules',
+    'information',
+    'team',
+    'read',
+    'must-read',
+    'ongoing',
+    'scheduled',
+    'completed',
+    'suggestions',
+  ];
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (typeof id === 'string' && !page.includes(id)) {
+      navigate('/404');
+    }
+  }, [id, navigate]);
+
   return (
     <>
       <FlexBox>
         <Sidebar url={url} collectionName={collectionName}></Sidebar>
         <Content></Content>
       </FlexBox>
+      <Footer />
     </>
   );
 }
